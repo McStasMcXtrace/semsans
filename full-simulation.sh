358dd509e576159a7368af902ec7ef306ead1dac
Thom van der Woude <tbvanderwoude@gmail.com>
2024-05-16 21:55:36 +0200

    Added way to turn on OpenACC GPU support to full-simulation.sh

--- full-simulation.sh
+++ full-simulation.sh
@@ -3,8 +3,8 @@
 # ./full-simulation.sh 100000 4 5 0,0.020908 foil.instr foil_empty.instr
 
 # Check if the number of arguments provided is correct
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <N> <N_threads> <N_steps> <B_min,B_max> <instr_with_sample> <instr_no_sample>"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 <N> <N_threads> <N_steps> <B_min,B_max> <instr_with_sample> <instr_no_sample> <mode>\nmode can be CPU or GPU" 
     exit 1
 fi
 
@@ -15,6 +15,7 @@ N_steps=$3
 By_range=$4
 instr_with_sample=$5
 instr_no_sample=$6
+mode=$7
 
 # Generate timestamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -31,7 +32,16 @@ rm -rf "${dir_name}_${timestamp}"
     
 mkdir data
 cd instr
-mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/up"
-mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
-mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
-mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
+
+if [ $mode == 'CPU' ]; then
+    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
+    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
+    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
+    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
+else
+    echo "Compiling with OpenACC GPU acceleration, this will require the CUDA toolkit to be installed"
+    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
+    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
+    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
+    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=2.165 DL=0.02 By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
+fi
