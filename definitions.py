9f01f6aacbd618d1049e0a2eff40bc179a59e64d
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-06-02 15:03:59 +0200

    Integrated envelope and sampling limits into computation of delta range in Instrument class

--- definitions.py
+++ definitions.py
@@ -1,6 +1,10 @@
 import numpy as np
 import re
 
+# Factor for converting a sigma to a FWHM and back
+FWHM_factor = 2 * np.sqrt(2 * np.log(2))
+
+detector_pixel_size = 10e-6 # m
 # Sample volume thickness
 t = 0.001 # m
 # Radius of sphere, equal to 1 um but expressed in AA
@@ -12,6 +16,7 @@ delta_rho = 6e10 * (1e2) ** 2 # 1/m^2 (?)
 c = 4.63e14 # T-1 m-2
 theta_0 = np.deg2rad(5.5) # rad
 wavelength = 2.165e-10 # m
+L_s = 1.8 # m
 
 
 def s_t(R = R, t = t, wavelength = wavelength, phi=phi, delta_rho=delta_rho):
@@ -34,7 +39,6 @@ def compute_z(By, theta_0 = theta_0, wavelength=wavelength, L_s = 1.8):
 # Computes By given z and other fixed parameters
 def compute_By(z):
     # Distance from detector to sample
-    L_s = 1.8 # m
     delta_B = np.pi * np.tan(theta_0) * z / (c * wavelength ** 2 * L_s)
     return delta_B
 
@@ -58,21 +62,27 @@ def G_norm(z, R):
 def compute_P_dark_field(I_up,I_down):
     return  (I_up - I_down) / (I_up + I_down)
 
+
 # Data processing helper functions
-def extract_By(file):
-    with open(file) as f:
-        text = f.read()
-        
-        match = re.search(r'# Param: By=([0-9.]+)', text)
-        assert(match)
-        return float(match.group(1))
-    
+def extract_parameters(file):
+    parameters = {}
+    pattern = re.compile(r'# Param:\s*(\w+)=([\d\.]+)')
+    with open(file, 'r') as file:
+        for line in file:
+            match = pattern.search(line)
+            if match:
+                param_name = match.group(1)
+                param_value = float(match.group(2))
+                parameters[param_name] = param_value
+    return parameters
+
 def get_data(i, folder='data'):
     y, I_up = np.genfromtxt(f'{folder}/up/{i}/det.dat', delimiter=' ', usecols=(0,1), unpack=True)
     _, I_down = np.genfromtxt(f'{folder}/down/{i}/det.dat', delimiter=' ', usecols=(0,1), unpack=True)
     _, I_empty_up = np.genfromtxt(f'{folder}/empty_up/{i}/det.dat', delimiter=' ', usecols=(0,1), unpack=True)
     _, I_empty_down = np.genfromtxt(f'{folder}/empty_down/{i}/det.dat', delimiter=' ', usecols=(0,1), unpack=True)
-    By = extract_By(f'{folder}/up/{i}/det.dat')
+    parameters = extract_parameters(f'{folder}/up/{i}/det.dat')
+    By = parameters['By']
     return y, I_up, I_down, I_empty_up, I_empty_down, By
 
 def indices_within_range(x, a, b):
