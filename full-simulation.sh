80ce6f694cc172e630307da24b2778f033281e56
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-06-23 23:39:48 +0200

    Added L_s to full-simulation.sh

--- full-simulation.sh
+++ full-simulation.sh
@@ -3,8 +3,8 @@
 # ./full-simulation.sh 100000 4 5 0,0.020908 2.165 0.02 10000 0.001 0.095993 iso iso_empty GPU
 
 # Check if the number of arguments provided is correct
-if [ "$#" -ne 12 ]; then
-    echo "Usage: $0 <N> <N_threads> <N_steps> <B_min,B_max> <L0> <DL> <R> <t> <theta0_foil> <instr_with_sample> <instr_no_sample> <mode>\nmode can be CPU or GPU" 
+if [ "$#" -ne 13 ]; then
+    echo "Usage: $0 <N> <N_threads> <N_steps> <B_min,B_max> <L0> <DL> <R> <t> <theta0_foil> <L_s> <instr_with_sample> <instr_no_sample> <mode>\nmode can be CPU or GPU" 
     exit 1
 fi
 
@@ -18,9 +18,10 @@ DL=$6
 R=$7
 t=$8
 theta0_foil=$9
-instr_with_sample=${10}
-instr_no_sample=${11}
-mode=${12}
+L_s=${10}
+instr_with_sample=${11}
+instr_no_sample=${12}
+mode=${13}
 
 # Generate timestamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -39,14 +40,14 @@ mkdir data
 cd instr
 
 if [ $mode == 'CPU' ]; then
-    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
-    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
-    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
-    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
+    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
+    mcrun $instr_with_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
+    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
+    mcrun $instr_no_sample --mpi=$N_threads -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
 else
     echo "Running with OpenACC GPU acceleration, this will require the CUDA toolkit to be installed"
-    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
-    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
-    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
-    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
+    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=1 --dir "../${dir_name}/up" 
+    mcrun $instr_with_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=-1 --dir "../${dir_name}/down"
+    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=1 --dir "../${dir_name}/empty_up"
+    mcrun $instr_no_sample --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t theta0=$theta0_foil L_s=$L_s By=$By_range AnaSign=-1 --dir "../${dir_name}/empty_down"
 fi
