abe27a0d8241c1ed92b82f3b5c7a313b17894c9e
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-06-26 23:20:53 +0200

    Extended instrument model to also have min, max L_s with a more maximal delta range assuming variable L_s

--- instrument.py
+++ instrument.py
@@ -1,9 +1,7 @@
 import numpy as np
 from definitions import *
 FWHM_env_min = 2e-3
-# L_s_reduction = 0.75
-# For simulating variable L_s
-# L_s_min = 1.5 
+
 class Instrument:
     def __init__(self, id: str, name: str, prec_type: str, L0: float, DL: float, theta_0: float, By_min: float, By_max: float, L_s: float = 1.8, L_1: float = 4, L_2: float = 2, detector_size = detector_size):
         self.id = id
@@ -54,7 +52,7 @@ class Instrument:
         # Due to the focussing condition, one component will have field By_max/2 and the other By_max, giving a delta of By_max/2
         return compute_z(self.By_max * (1 - self.L_2 / self.L_1), self.theta_0, self.L0, self.L_s)
 
-    def delta_max_sampling(self, samples = 10):
+    def delta_max_sampling(self, samples = 5):
         f_s = 1/detector_pixel_size
         f_super_sampled = f_s / samples
         delta_max_sampling = f_super_sampled * self.L0 * self.L_s
@@ -69,6 +67,11 @@ class Instrument:
     
     def delta_range(self):
         return self.delta_min(), self.delta_max()
+    
+    def delta_range_var_L_s(self):
+        L_s_min, L_s_max = self.L_s_min(), self.L_s_max()
+        d_min, d_max = self.delta_min(), self.delta_max()
+        return L_s_min / self.L_s * d_min, L_s_max / self.L_s * d_max
     
     def Q_max(self):
         return np.pi / self.delta_min_detector()
@@ -86,7 +89,15 @@ class Instrument:
     
     def theta_a(self):
         return np.arctan(self.detector_size / (2 * self.L_s))
-
+    
+    def L_s_min(self, theta_a_max = 15e-3):
+        return self.detector_size / (2 * np.tan(theta_a_max))
+    
+    def L_s_max(self, d_p = 0.3, t_max = 0.01):
+        return self.L_2 - d_p / 2 - t_max/2
+    
+    def L_s_range(self):
+        return self.L_s_min(), self.L_s_max()
             
     def __str__(self):
         d_max_field = self.delta_max_B_field()
@@ -103,6 +114,8 @@ class Instrument:
         Q_max = self.Q_max()
         theta_a = self.theta_a() * 1e3
         R_max = 1 / (Q_max * 1e-10)
+        L_s_min, L_s_max = self.L_s_range()
+        delta_min_var, delta_max_var = self.delta_range_var_L_s()
         return f"""Instrument ID {self.id}; Name: {self.name}
     \tSource: L0 = {self.L0 * 1e10} Å; sigma_L = {self.DL * 1e10} Å
     \tPrecession device: {self.prec_type_long()}; theta_0 = {round(self.theta_0,2)} rad; By_min = {self.By_min * 1e3}mT; By_max = {self.By_max * 1e3}mT
@@ -114,12 +127,14 @@ class Instrument:
     \tQ_max as determined by detector height and distance: {round(Q_max * 1e-10, 7)} Å-1
     \tApproximate max R of solid sphere sample: {round(R_max * 0.1, 2)} - {round(R_max,2)} nm
     \ttheta_a: {round(theta_a, 2)} mrad
-    \tfinal δ range: {round(d_min * 1e9,1)} - {round(d_max * 1e9, 1)}nm ({min_name} - {max_name} limited)"""
+    \tL_s range: {round(L_s_min, 2)} - {round(L_s_max, 2)} m
+    \tfinal δ range: {round(d_min * 1e9,1)} - {round(d_max * 1e9, 1)}nm ({min_name} - {max_name} limited)
+    \tfinal δ range with variable L_s: {round(delta_min_var * 1e9,1)} - {round(delta_max_var * 1e9, 1)}nm"""
     
 def print_latex_tables(instrs):
     for i in [0,1]:
         for instr in instrs[3:-6]:
-            # print(instr)
+            print(instr)
             delta_max_field = instr.delta_max_B_field()
             delta_max_env = instr.delta_max_envelope()
             delta_max_ten_samples = instr.delta_max_sampling()
