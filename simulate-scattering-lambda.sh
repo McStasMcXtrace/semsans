d661e9a1fe62199a81bfe2cbc3745d28e37ce5dc
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-06-26 14:45:28 +0200

    Wrote simulation and analysis code for getting base scattering patterns for detector as function of lambda

---
+++ simulate-scattering-lambda.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Example usage:
+# ./simulate-instr.sh 100000 4 0.020908 2.165 0.02 iso.instr GPU
+
+# Check if the number of arguments provided is correct
+if [ "$#" -ne 9 ]; then
+    echo "Usage: $0 <N> <N_threads> <mode> <R> <t> <L_s> <L0_range> <DL> <N>\nmode can be CPU or GPU"
+    exit 1
+fi
+
+# Assign command-line arguments to variables
+N=$1
+N_threads=$2
+mode=$3
+R=$4
+t=$5
+L_s=$6
+L0=$7
+DL=$8
+N_steps=$9
+
+dir_name="scattering_data"
+rm -rf "${dir_name}"
+mkdir -p "${dir_name}"
+
+
+cd instr
+
+if [ $mode == 'CPU' ]; then
+    mcrun sample_scattering --mpi=$N_threads -N $N_steps  -n $N L0=$L0 DL=$DL R=$R t=$t L_s=$L_s --dir "../${dir_name}/${R}-${t}"
+else
+    echo "Running with OpenACC GPU acceleration, this will require the Nvidia HPC toolkit to be installed"
+    mcrun sample_scattering --openacc -n $N -N $N_steps L0=$L0 DL=$DL R=$R t=$t L_s=$L_s --dir "../${dir_name}/${R}"
+fi
---
+++ simulate-scattering.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./simulate-scattering-lambda.sh 100000000 4 GPU 3000 0.01 1.8 3,10.0 0.0001 100
+./simulate-scattering-lambda.sh 100000000 4 GPU 3000 0.01 1.8 3,10.0 0.0001 100
+./simulate-scattering-lambda.sh 100000000 4 GPU 3000 0.01 1.8 3,10.0 0.0001 100
+./simulate-scattering-lambda.sh 100000000 4 GPU 3000 0.01 1.8 3,10.0 0.0001 100
